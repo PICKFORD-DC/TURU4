dd3c53cd7389234de295abcc94518395cbf5e605
PICKFORD-DC <105628212+PICKFORD-DC@users.noreply.github.com>
2022-10-07 22:31:45 +0700

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -103,7 +103,7 @@ global.fakefb = {
     } }
 		// Fake ðŸ¤¥
 		global.ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
-		global.fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': 'PICK FORD' GANTENG ;-;', 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;PICK-FORD,;;;\nFN:PICKFORD,\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./thumbnail.jpg'), thumbnail: fs.readFileSync('./thumbnail.jpg'),sendEphemeral: true}}}
+		global.fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': 'PICK FORD GANTENG ;-;', 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;PICK-FORD,;;;\nFN:PICKFORD,\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./thumbnail.jpg'), thumbnail: fs.readFileSync('./thumbnail.jpg'),sendEphemeral: true}}}
         global.fvn = {
             key: { 
                  fromMe: false,
